7035c424afed439fc7d32371de8319abe1aa098a
Leon Heuer <leon.heuer@outlook.de>
2022-12-11 13:57:53 +0100

    feat: get rid of Rollbar

--- src/main/java/de/skycave/skycavelib/SkyCaveLib.java
+++ src/main/java/de/skycave/skycavelib/SkyCaveLib.java
@@ -1,22 +1,12 @@
 package de.skycave.skycavelib;
 
-import com.rollbar.notifier.Rollbar;
-import com.rollbar.notifier.config.Config;
-import com.rollbar.notifier.config.ConfigBuilder;
 import de.skycave.skycavelib.models.SkyCavePlugin;
 
 public final class SkyCaveLib extends SkyCavePlugin {
 
-    private static Rollbar rollbar;
-
     @Override
     public void onEnable() {
         super.onEnable();
-        Config config = ConfigBuilder.withAccessToken(System.getenv("ROLLBAR_ACCESS_TOKEN"))
-                .environment("production")
-                .codeVersion("1.3.0")
-                .build();
-        rollbar = Rollbar.init(config);
     }
 
     @Override
@@ -24,7 +14,4 @@ public final class SkyCaveLib extends SkyCavePlugin {
         super.onDisable();
     }
 
-    public static Rollbar remoteLogger() {
-        return rollbar;
-    }
 }
